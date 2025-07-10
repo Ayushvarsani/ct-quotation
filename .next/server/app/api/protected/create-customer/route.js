9d47990ceb91581f25fcc068fdea5ef663b75b92
yuvrajsyintacting <yuvraj.sy@intacting.in>
2025-07-10 22:13:20 +0530

    updates

--- .next/server/app/api/protected/create-customer/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/protected/create-customer/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_22719e50._.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__bfc77e49._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/protected/create-customer/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/protected/create-customer/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/protected/create-customer/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
